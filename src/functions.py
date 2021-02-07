8706bc8b86dbdf76c7419da611320dab2ba60ca4
altcp <alvin.tan@ttrtea.com>
2021-02-08 04:06:24 +0800

    Introduce SVR

--- src/functions.py
+++ src/functions.py
@@ -12,11 +12,15 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import pmdarima as pm
+import matplotlib.dates as mdates
 import matplotlib.pyplot as plt
 import pandas_datareader.data as web
 
 from pmdarima.arima import ndiffs
-from statsmodels.tsa.arima_model import ARIMA
+from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.svm import SVR
 
 
 #Load Configuration
@@ -41,17 +45,16 @@ def get_eda(dashboard_required=False, univariate=True):
     TD = config['settings']['tech_disruptor']
 
     if (TD == True):
-        
-        features, target, bins = process_data(dashboard_required, univariate, TD)
+        predictions, outlook, outcome = process_data(dashboard_required, univariate, TD)
     else:
 
         print(" ")
         print(" ")
         print("Sell Side Quantative Research (SSQR)")
         print("Next Business Day Outlook, 0.0.1")
-        features, target, bins = process_data(dashboard_required, univariate)
+        predictions, outlook, outcome = process_data(dashboard_required, univariate)
     
-    return features, target, bins
+    return predictions, outlook, outcome
 
 
 
@@ -140,11 +143,11 @@ def process_data(display_eda, univariate, tech_disruptor=False):
 
         fit = pd.DataFrame()    
         fit[target] = df1[target].shift(-1)
-        #print(df1)
-        
+        fit['Date'] = df1.index
         fit['Log Returns'] = np.log(fit[target]/fit[target].shift(1))
         fit['Outcome'] = pd.cut(x=fit['Log Returns'], bins=[fit['Log Returns'].min(), -(small_gain), 0, small_gain, fit['Log Returns'].max()], 
-                           labels=['big loss', 'small loss', 'small gain', 'big gain'])
+                                labels=['big loss', 'small loss', 'small gain', 'big gain'])
+        
         
         #Dataframe For Visualization
         for i in range(max_number_of_lags):
@@ -155,23 +158,29 @@ def process_data(display_eda, univariate, tech_disruptor=False):
                 fit[column_name] = df1[target].shift(i)
         
         fit1 = fit.dropna().reset_index(drop=True)
-        fit_y = fit1[[target, 'Log Returns', 'Outcome']]
-        fit_x = fit1.drop([target, 'Log Returns', 'Outcome'], axis=1)
-        fit_bins = fit['Outcome']
+        fit2 = fit1[['Date', target, 'Log Returns', 'Outcome']] 
+        fit_svr = fit1.drop(['Date', 'Outcome'], axis=1)
+        #print(fit_svr)
         
-        #Train-Test Split
-        train_returns = fit_y['Log Returns'].head(int(len(fit_y) * (1 - test_size))) 
-        test_returns = fit_y['Log Returns'].tail(int(len(fit_y) * test_size))
         
-        #Call Baseline
-        baseline_model, pred_df['Box-Jenkins'], outlook_df['SARIMAX'] = baseline_r_model(train_returns, test_returns, max_number_of_lags, 
-                                                                                          max_number_of_diff, frequency, max_moving_average_window) 
+        #Baseline Model
+        train_returns = fit2['Log Returns'].head(int(len(fit2) * (1 - test_size))) 
+        test_returns = fit2['Log Returns'].tail(int(len(fit2) * test_size)).reset_index(drop=True)
+        baseline_model, pred_df['SARIMAX'], outlook_df['SARIMAX'] = baseline_r_model(train_returns, test_returns, 
+                                                                                     max_number_of_lags, max_number_of_diff, frequency, max_moving_average_window)
         
+        #SVR
+        train_returns = fit_svr.head(int(len(fit_svr) * (1 - test_size))) 
+        test_returns = fit_svr.tail(int(len(fit_svr) * test_size)).reset_index(drop=True)
+        pred_df['SVR'], outlook_df['SVR'] = svr_r_model(train_returns, test_returns, fit_svr, target)
+        #print(pred_df)
+    
+    
         #Display 
         if (display_eda):
             print(" ")
             print(" ")
-            print(fit1)
+            print(fit2)
             print(" ")
             print(" ")
             print(baseline_model.summary())
@@ -187,13 +196,12 @@ def process_data(display_eda, univariate, tech_disruptor=False):
 
             print(" ")
         
-        
     else:
         #Multivariate
         print(" ")
 
    
-    return fit_x, fit_y, fit_bins
+    return pred_df, outlook_df, fit2
 
 
 
@@ -224,9 +232,47 @@ def baseline_r_model(train_set, test_set, p, d, m, q):
     outlook_array = optimized_model.predict(n_periods=1)
     outlook_list = outlook_array.tolist()[0]
     outlook.append(outlook_list)
-    
+
     return optimized_model, predictions, outlook
 
 
+
+#Support Vector Regression, 1995 
+def svr_r_model(train_set, test_set, full_set, target):
+    
+    #Preprocessing
+    train_y = np.asarray(train_set['Log Returns']) 
+    train_x = np.asarray(train_set.loc[:, train_set.columns.difference(['Log Returns', target])])
+    
+    test_y = np.asarray(test_set['Log Returns']) 
+    test_x = np.asarray(test_set.loc[:, test_set.columns.difference(['Log Returns', target])])
+    
+    full_y = np.asarray(full_set['Log Returns']) 
+    full_x = np.asarray(full_set.loc[:, full_set.columns.difference(['Log Returns', target])])
+    
+    column_name = 'X' + str(len(test_set.columns)-3) 
+    outlook_x = np.asarray(test_set.loc[:, test_set.columns.difference(['Log Returns', column_name])])
+
+    predictions = []
+    outlook = []
+    
+    #Measure the Performance
+    pipe = Pipeline(steps=[('scaler', StandardScaler()), ('svr', SVR())])
+    param_grid={
+            'svr__kernel':['rbf', 'poly'],
+            'svr__C': [1, 100, 1000],
+            'svr__epsilon': [0.0001, 0.0005, 0.001]
+        }
+    
+    search = GridSearchCV(pipe, param_grid, n_jobs=-2)
+    search.fit(train_x, train_y)
+    predictions = search.predict(test_x)
+    
+    #Make the Forecast
+    search.fit(full_x, full_y)
+    outlook = search.predict(outlook_x)
+   
+    return predictions, outlook[-1]
+    
 
 
